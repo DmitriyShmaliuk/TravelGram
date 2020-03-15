6d7c893afe5e1235700cef805c56e98cfdbb5dba
Dmitriy Shmaliuk <dimkas2001@icloud.com>
2020-03-15 20:40:49 +0200

    add (scripts/index.js): functins for connect and showing google-maps

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,17 @@
-const bigSliderStateButtons = document.querySelectorAll('.big-slider [type=radio]')
+const bigSliderStateButtons = document.querySelectorAll('.big-slider [type=radio]');
+const mapOptions = {
+    coordinates: {
+        lat: 50.254650,
+        lng: 28.658667
+    },
+    zoom: 13,
+};
+
+loadMaps()
+    .then(()=>initMap(mapOptions))
+    .catch((error)=>{
+        console.log(error);
+    });
 
 Array.prototype.forEach.call(bigSliderStateButtons, (el)=>{
     el.addEventListener('click', changeBigSliderState, true);
@@ -43,9 +56,62 @@ function setNewClass (currentClass, sliderContent, id){
     sliderContent.classList.add(sliderClasses[id]);
 }
 
+function loadMaps(){
+    return new Promise((resolve, reject)=>{
+        const script = document.createElement('script');
+        script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBqKlMjrbp7YV-Q_MfSbmu336Rg1HjnJUo&callback=initMap';
+        document.body.append(script);
 
+        script.onload = () => resolve();
+        script.onerror = () => reject(new Error (`Script can not ${script.src} be connected!!!`));
+    });
+}
 
+function initMap(options) {
+    const map = new google.maps.Map(document.getElementsByClassName("map")[0], options);
+    showCurrentLocation(map);
+}
 
+function showCurrentLocation(map){
+    if (navigator.geolocation) {
+        const infoWindow = new google.maps.InfoWindow;
 
+        navigator.geolocation.getCurrentPosition(position => successGetLocation(map, position),
+           () => errorGetLocation(map, infoWindow));
+    }
+    else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
 
+function successGetLocation (map, position) {
+    const currentPosition = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    };
 
+    createMarker(map, currentPosition, "Your current position");
+    map.setCenter(currentPosition);
+}
+
+function errorGetLocation(map, infoWindow){
+    handleLocationError(true, infoWindow, map.getCenter());
+}
+
+function createMarker(map, position, title){
+    return new google.maps.Marker({
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position,
+        map,
+        title
+    });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
